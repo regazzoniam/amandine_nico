e6aeccc3ef46fdb933cf663ea39a606842f3fc4c
regazzoniam <amandineregad@gmail.com>
2022-05-06 12:24:44 +0200

    update account controller function createNewAccount

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,24 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Form\AccountType;
 use App\Repository\AccountRepository;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
     public function __construct(private AccountRepository $accountRepository) { }
-
-    #[Route('/utilisateurs/{slug}', name: 'app_account_show')]
-    public function userBySlug($slug): Response
-    {
-        $userEntity = $this->accountRepository->getOneAccountBySlug($slug);
-
-        return $this->render('account/index.html.twig', [
-            'userEntity' => $userEntity,
-        ]);
-    }
 
     #[Route('/utilisateurs/{slug}/commentaires', name: 'app_account_comments_show')]
     public function userBySlugAllComments($slug): Response
@@ -40,4 +36,35 @@ class AccountController extends AbstractController
 
         ]);
     }
+
+    #[Route('/utilisateurs/nouveau', name: 'app_account_new_show')]
+    public function show(Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(AccountType::class, $account = new Account());
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            // dd($form->getData());
+            $account->setCreatedAt(new DateTime());
+            $em->persist($form->getData());
+            $em->flush();
+            return $this->redirectToRoute('app_accounts');
+        }
+
+        return $this->render('account/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    //on met la fonction ici car la route avec paramètre rentre en conflit avec la route avec /utilisateur/nouveau
+    #[Route('/utilisateurs/{slug}', name: 'app_account_show')]
+    public function userBySlug($slug): Response
+    {
+        $userEntity = $this->accountRepository->getOneAccountBySlug($slug);
+
+        return $this->render('account/index.html.twig', [
+            'userEntity' => $userEntity,
+        ]);
+    }
+
 }
--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Account;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,36 @@ class AccountType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('nickname')
-            // ->add('wallet')
-            ->add('slug')
-            ->add('createdAt')
+            ->add('name', TextType::class, [
+                'label'=> false,
+                'attr'=>[
+                    'placeholder' => 'Nom'
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'label'=> false,
+                'attr'=>[
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add('nickname', TextType::class, [
+                'label'=> false,
+                'attr'=>[
+                    'placeholder' => 'Pseudo'
+                ]
+            ])
+            ->add('slug', TextType::class, [
+                'label'=> false,
+                'attr'=>[
+                    'placeholder' => 'Slug'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'=>'Envoyer',
+                'attr'=>[
+                    'class' => 'btn btn-green'
+                ]
+            ])
         ;
     }
 
